603950b85c0bd6bff71735e44bc2a097917117ab
Nick DeRobertis <whoopnip@gmail.com>
2020-03-19 12:17:58 -0400

    Setup app demo

--- maps-example/src/app/app.component.ts
+++ maps-example/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+/// <reference path="../../node_modules/@types/googlemaps/index.d.ts" />
+import { Component, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'maps-example';
+export class AppComponent implements AfterViewInit {
+  title = "maps-example";
+  @ViewChild("mapContainer", { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.73061;
+  lng = -73.935242;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8
+  };
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map
+  });
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+  }
 }
